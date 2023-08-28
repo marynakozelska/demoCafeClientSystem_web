bd4a2ac47df4d9ba21b7e62bfd62d315cec2f5e7
Mary Kozelska <maryswan811@gmail.com>
2023-08-28 12:25:03 +0300

    Add active and previous orders to profile

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -3,6 +3,7 @@ import {CustomerService} from "../../services/customer.service";
 import {User} from "../../entities/user";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
+import {Order} from "../../entities/order";
 
 @Component({
   selector: 'app-customer',
@@ -12,6 +13,8 @@ import {Router} from "@angular/router";
 export class CustomerComponent {
   message: string;
   userData: User = {firstName: '', email: '', password: '', role: ''};
+  activeOrders: Order[];
+  previousOrders: Order[];
 
   constructor(public router: Router,
               private service: CustomerService,
@@ -23,6 +26,8 @@ export class CustomerComponent {
 
   ngOnInit() {
     this.getInfo();
+    this.getActiveOrders();
+    this.getPreviousOrders();
   }
 
   public getInfo(): void {
@@ -34,5 +39,19 @@ export class CustomerComponent {
 
   public logout(): void {
     this.authService.logout();
+  }
+
+  public getActiveOrders(): void {
+    this.authService.getActiveCustomerOrders()
+      .subscribe((response: Order[]) => {
+        this.activeOrders = response;
+      });
+  }
+
+  public getPreviousOrders(): void {
+    this.authService.getPreviousOrders()
+      .subscribe((response: Order[]) => {
+        this.previousOrders = response;
+      });
   }
 }
